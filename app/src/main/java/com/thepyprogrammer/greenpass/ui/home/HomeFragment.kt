1272f1fa1abf95cf842d551d00be3d250c7ce9b5
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-24 21:15:25 +0800

    Fix HomeFragment

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/home/HomeFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/home/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.thepyprogrammer.greenpass.ui.R
+import com.thepyprogrammer.greenpass.R
 
 class HomeFragment : Fragment() {
 
