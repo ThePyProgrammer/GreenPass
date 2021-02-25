61f602bee7054d812d41ffbe8f77af880ca310f0
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-25 11:40:40 +0800

    Update LoginRegisterActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/loginRegister/LoginRegisterActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/loginRegister/LoginRegisterActivity.kt
@@ -6,8 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.thepyprogrammer.greenpass.R
 import com.thepyprogrammer.greenpass.ui.loginRegister.ui.main.SectionsPagerAdapter
 
 class LoginRegisterActivity : AppCompatActivity() {
