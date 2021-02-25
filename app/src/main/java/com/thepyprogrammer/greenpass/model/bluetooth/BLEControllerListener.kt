4f4023f2aa42ee2c0637c71e445b961a50b9d543
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-25 21:21:45 +0800

    Delete BLEControllerListener.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/bluetooth/BLEControllerListener.kt
+++
@@ -1,7 +0,0 @@
-package com.thepyprogrammer.greenpass.model.bluetooth
-
-interface BLEControllerListener {
-    fun BLEControllerConnected()
-    fun BLEControllerDisconnected()
-    fun BLEDeviceFound(name: String?, address: String?)
-}
