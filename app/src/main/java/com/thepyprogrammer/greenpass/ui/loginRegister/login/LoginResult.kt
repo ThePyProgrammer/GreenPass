b316c16d94a8a5a2cf36fa39895690eafd1e5dbc
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-26 14:43:18 +0800

    Delete ui.loginRegister.login subpackage

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/loginRegister/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.thepyprogrammer.greenpass.ui.loginRegister.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
