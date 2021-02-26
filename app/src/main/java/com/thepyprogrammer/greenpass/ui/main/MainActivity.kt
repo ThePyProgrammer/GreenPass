fb2435fea94b90370080448ec88670740f5dbbba
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-27 00:15:04 +0800

    Cleanup MainActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/MainActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.thepyprogrammer.greenpass.ui.main
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
