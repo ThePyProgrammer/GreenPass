a272460a8ccf1c24a4d5a77282096bc20f41f17e
Edden Chew <64969649+exdan-is-a-noob-at-io@users.noreply.github.com>
2021-02-26 16:25:09 +0800

    Create ImageDetailsActivity.kt

---
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/profile/ImageDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.thepyprogrammer.greenpass.ui.main.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thepyprogrammer.greenpass.R
+
+class ImageDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_details)
+    }
+}
