d0dd5412756a66206deee2e0da3c5cdd0d95d916
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 15:56:52 +0800

    Cleanup VaccinatedPerson.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedPerson.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedPerson.kt
@@ -3,8 +3,8 @@ package com.thepyprogrammer.greenpass.model.account
 import java.time.LocalDate
 
 data class VaccinatedPerson(
-    val name: String,
-    val ic: String,
-    val email: String,
-    val dateOfVaccine: LocalDate
+        val name: String,
+        val ic: String,
+        val email: String,
+        val dateOfVaccine: LocalDate
 )
