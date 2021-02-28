e48bbb740ba790a6f8630d6923a1c7b3d8209f87
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 19:01:37 +0800

    Update SplashActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/splash/SplashActivity.kt
@@ -3,11 +3,13 @@ package com.thepyprogrammer.greenpass.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.thepyprogrammer.greenpass.R
 import com.thepyprogrammer.greenpass.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -16,6 +18,16 @@ class SplashActivity : AppCompatActivity() {
             startActivity(home)
             finish()
         }, SPLASH_TIME_OUT.toLong())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window?.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        window?.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 
     companion object {
