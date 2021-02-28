eb33331fb8140d300cc6ec9666fffbf6c7f0cca1
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 21:47:44 +0800

    Cleanup LoggedInUser.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/account/LoggedInUser.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/account/LoggedInUser.kt
@@ -4,6 +4,6 @@ package com.thepyprogrammer.greenpass.model.account
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
 data class LoggedInUser(
-        val userId: String,
-        val displayName: String
+    val userId: String,
+    val displayName: String
 )
