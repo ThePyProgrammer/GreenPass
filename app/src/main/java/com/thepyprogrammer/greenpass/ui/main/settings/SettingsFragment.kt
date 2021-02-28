ea7bd4e855e8410525d0ce4e6c2300e65d35c5a2
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 22:05:58 +0800

    Update SettingsFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
@@ -1,12 +1,19 @@
 package com.thepyprogrammer.greenpass.ui.main.settings
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.thepyprogrammer.greenpass.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        val shakePreference: SwitchPreference? = findPreference("shake")
+        shakePreference?.setOnPreferenceChangeListener { preference: Preference, any: Any ->
+            
+            true
+        }
     }
 }
