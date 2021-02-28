dbef1c8c3438c8b52424fbaa9596dd7106e1dd8a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 21:47:53 +0800

    Update LoginResult.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/account/LoginResult.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/account/LoginResult.kt
@@ -4,6 +4,6 @@ package com.thepyprogrammer.greenpass.ui.account
  * Authentication result : success (user details) or error message.
  */
 data class LoginResult(
-        val success: LoggedInUserView? = null,
-        val error: Int? = null
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
 )
