9473f0e38f59eeae96b0b66c1d4d672658e2eea2
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 21:49:12 +0800

    Delete ui.account subpackage

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/account/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.thepyprogrammer.greenpass.ui.account
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
