515c23e9280356d79a5e6d512e346c6ab4e24444
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 17:07:40 +0800

    Update AndroidManifest.xml

---
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/intro/IntroActivity.kt
@@ -0,0 +1,10 @@
+package com.thepyprogrammer.greenpass.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
