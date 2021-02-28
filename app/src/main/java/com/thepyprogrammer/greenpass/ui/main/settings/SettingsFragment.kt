a8e96ffdc887b157fd8212023fa62552070f202a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 15:57:45 +0800

    Cleanup SettingsFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
@@ -1,13 +1,7 @@
 package com.thepyprogrammer.greenpass.ui.main.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceFragmentCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.thepyprogrammer.greenpass.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
