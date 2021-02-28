f7ac843db130d3e1df99f849abef833ce5e8cc09
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 17:27:58 +0800

    Update IntroActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/intro/IntroActivity.kt
@@ -1,6 +1,21 @@
 package com.thepyprogrammer.greenpass.ui.intro
 
 import agency.tango.materialintroscreen.MaterialIntroActivity
+import agency.tango.materialintroscreen.SlideFragmentBuilder
+import android.os.Bundle
+import com.thepyprogrammer.greenpass.R
 
 
-class IntroActivity : MaterialIntroActivity()
+class IntroActivity : MaterialIntroActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addSlide(
+            SlideFragmentBuilder()
+                .backgroundColor(R.color.fourth_slide_background)
+                .buttonsColor(R.color.fourth_slide_buttons)
+                .title("That's it")
+                .description("Would you join us?")
+                .build()
+        )
+    }
+}
