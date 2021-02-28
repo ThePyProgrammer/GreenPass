60a93678f8fc931c579cbc483f9223fecc690a50
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 17:16:38 +0800

    Cleanup IntroActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/intro/IntroActivity.kt
@@ -1,15 +1,6 @@
 package com.thepyprogrammer.greenpass.ui.intro
 
 import agency.tango.materialintroscreen.MaterialIntroActivity
-import android.os.Bundle
-import androidx.annotation.Nullable
 
 
-class IntroActivity : MaterialIntroActivity() {
-    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-
-}
+class IntroActivity : MaterialIntroActivity()
