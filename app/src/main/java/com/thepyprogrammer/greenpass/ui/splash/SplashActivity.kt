5d8d4156d5d688e6fb58f39a8d8aeffc6d0d8b72
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-28 22:23:40 +0800

    Update SplashActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/splash/SplashActivity.kt
@@ -17,7 +17,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
         val binding = ActivitySplashBinding.inflate(layoutInflater)
 
         val backgroundImage: ImageView = binding.splashScreenImage
