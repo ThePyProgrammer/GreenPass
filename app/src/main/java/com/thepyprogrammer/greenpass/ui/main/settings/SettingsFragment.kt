5ba5ef994016b1405732a266ff69398339e4756c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-01 20:14:52 +0800

    Update SettingsFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.thepyprogrammer.greenpass.ui.main.settings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
@@ -14,8 +15,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         val shakePreference: SwitchPreferenceCompat? = findPreference("shake")
-        shakePreference?.setOnPreferenceChangeListener { preference: Preference, any: Any ->
+        val themePreference: SwitchPreferenceCompat? = findPreference("theme")
+
+        shakePreference?.setOnPreferenceChangeListener { _: Preference, any: Any ->
             (activity as MainActivity).shakeToOpen = any as Boolean
+            true
+        }
+
+
+        themePreference?.setOnPreferenceChangeListener { _: Preference, any: Any ->
+            AppCompatDelegate.setDefaultNightMode(if (any as Boolean) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
             true
         }
     }
