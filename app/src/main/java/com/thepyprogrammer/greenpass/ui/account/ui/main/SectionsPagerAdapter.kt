368fc77c7ecef1139405ddf616e82a397c7bb815
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-04 11:55:42 +0800

    Update SectionsPagerAdapter.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/account/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/account/ui/main/SectionsPagerAdapter.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.thepyprogrammer.greenpass.R
+import com.thepyprogrammer.greenpass.ui.account.register.RegisterFragment
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
