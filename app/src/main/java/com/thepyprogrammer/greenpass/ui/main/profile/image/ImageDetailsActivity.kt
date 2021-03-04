22e66ab50c8be8e878402475c1f7bffae4ed0f7b
Edden Chew <64969649+exdan-is-a-noob-at-io@users.noreply.github.com>
2021-03-04 19:56:39 +0800

    Update ImageDetailsActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/profile/image/ImageDetailsActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/profile/image/ImageDetailsActivity.kt
@@ -1,13 +1,114 @@
 package com.thepyprogrammer.greenpass.ui.main.profile.image
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.thepyprogrammer.greenpass.R
+import com.thepyprogrammer.greenpass.ui.main.profile.image.ImagePickerActivity.Companion.showImagePickerOptions
+import com.thepyprogrammer.greenpass.ui.main.profile.image.ImagePickerActivity.PickerOptionListener
+import java.io.IOException
+
 
 class ImageDetailsActivity : AppCompatActivity() {
+    var REQUEST_IMAGE = 6969
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_details)
+    }
+
+    // my button click function
+    fun onProfileImageClick() {
+        Dexter.withActivity(this)
+            .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    if (report.areAllPermissionsGranted()) {
+                        showImagePickerOptions()
+                    } else {
+                        // TODO - handle permission denied case
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest?>?,
+                    token: PermissionToken
+                ) {
+                    token.continuePermissionRequest()
+                }
+            }).check()
+    }
+
+    private fun showImagePickerOptions() {
+        showImagePickerOptions(this, object : PickerOptionListener {
+            override fun onTakeCameraSelected() {
+                launchCameraIntent()
+            }
+
+            override fun onChooseGallerySelected() {
+                launchGalleryIntent()
+            }
+        })
+    }
+
+    private fun launchCameraIntent() {
+        val intent = Intent(this, ImagePickerActivity::class.java)
+        intent.putExtra(
+            ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION,
+            ImagePickerActivity.REQUEST_IMAGE_CAPTURE
+        )
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true)
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1) // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1)
+
+        // setting maximum bitmap width and height
+        intent.putExtra(ImagePickerActivity.INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, true)
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_WIDTH, 1000)
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_HEIGHT, 1000)
+        startActivityForResult(intent, REQUEST_IMAGE)
+    }
+
+    private fun launchGalleryIntent() {
+        val intent = Intent(this, ImagePickerActivity::class.java)
+        intent.putExtra(
+            ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION,
+            ImagePickerActivity.REQUEST_GALLERY_IMAGE
+        )
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true)
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1) // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1)
+        startActivityForResult(intent, REQUEST_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                val uri: Uri? = data?.getParcelableExtra("path")
+                try {
+                    // You can update this bitmap to your server
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+
+                    // loading profile image from local cache
+                    //todo fix this one loadProfile(uri.toString())
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 
 }
