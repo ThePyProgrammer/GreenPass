45700c79285c9f6a761e8936cdabf63cbecd8e13
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-04 11:53:49 +0800

    Update AccountActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/account/AccountActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/account/AccountActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import com.thepyprogrammer.greenpass.R
 import com.thepyprogrammer.greenpass.ui.account.ui.main.SectionsPagerAdapter
 
 class AccountActivity : AppCompatActivity() {
