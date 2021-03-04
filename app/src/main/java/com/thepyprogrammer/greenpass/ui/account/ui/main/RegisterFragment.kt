cf72185cbaecb9ca8bacec3c15903282522638c2
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-04 11:55:00 +0800

    Update RegisterFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/account/ui/main/RegisterFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/account/ui/main/RegisterFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.thepyprogrammer.greenpass.R
+import com.thepyprogrammer.greenpass.ui.account.PageViewModel
 
 /**
  * A placeholder fragment containing a simple view.
