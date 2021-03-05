2c66cba1207f93327f05420f94c12bb1d6fb5c96
Edden Chew <64969649+exdan-is-a-noob-at-io@users.noreply.github.com>
2021-03-05 17:17:47 +0800

    Update ProfileFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/profile/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.thepyprogrammer.greenpass.ui.main.profile
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,9 +11,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.transition.TransitionInflater
 import com.thepyprogrammer.greenpass.R
 import com.thepyprogrammer.greenpass.ui.main.profile.image.ImageDetailsActivity
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.imageView
+import kotlinx.android.synthetic.main.nav_header_main.*
+import java.io.File
+import java.lang.StringBuilder
+import java.util.*
 
 class ProfileFragment : Fragment() {
+
+    var circleImageView:CircleImageView? = null
+    var imageInfoFile: File? = null;
 
     companion object {
         fun newInstance() = ProfileFragment()
@@ -28,11 +38,17 @@ class ProfileFragment : Fragment() {
         return root
     }
 
+    override fun onStart() {
+        super.onStart()
+        loadImage()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val inflater = TransitionInflater.from(requireContext())
         enterTransition = inflater.inflateTransition(R.transition.slide_right)
         exitTransition = inflater.inflateTransition(R.transition.fade)
+        imageInfoFile = File(activity?.getFilesDir(), "profileImageURI.txt")
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -45,5 +61,33 @@ class ProfileFragment : Fragment() {
             startActivity(intent)
         }
     }
+
+    fun readData(): String {
+        if (!imageInfoFile!!.exists()){
+            return "";
+        }
+        val scanner = Scanner(imageInfoFile)
+        val string = StringBuilder(scanner.nextLine())
+
+        while (scanner.hasNextLine())
+            string.append("\n"+scanner.nextLine())
+
+
+        scanner.close()
+        return string.toString()
+    }
+
+    fun loadImage(){
+        var string: String  = readData();
+        if (!string.isEmpty()){
+            imageView!!.setImageURI(Uri.parse(readData()))
+        }
+        else{
+            imageView!!.setImageResource(R.drawable.edden_face)
+        }
+
+    }
+
+
 
 }
