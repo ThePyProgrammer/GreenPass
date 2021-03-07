84f4b4f501fc16675e41e08b2c0911fd4fe223eb
Prannaya Gupta <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-07 13:15:53 +0800

    Add LoginFragment view classes

---
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/account/login/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.thepyprogrammer.greenpass.ui.account.login.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
