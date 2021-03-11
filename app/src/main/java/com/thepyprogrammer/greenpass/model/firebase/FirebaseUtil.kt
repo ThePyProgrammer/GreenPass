3970831cb517869150c7f1061d9c9af3f5690294
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-11 23:47:20 +0800

    Update FirebaseUtil.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/firebase/FirebaseUtil.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/firebase/FirebaseUtil.kt
@@ -17,51 +17,24 @@ object FirebaseUtil {
     private var AUTH: FirebaseAuth? = null
     private var AUTH_UI: AuthUI? = null
 
-    // Connect to the Cloud Firestore emulator when appropriate. The host '10.0.2.2' is a
-    // special IP address to let the Android emulator connect to 'localhost'.
+    // Connect to the Cloud Firestore
     val firestore: FirebaseFirestore?
         get() {
-            if (FIRESTORE == null) {
-                FIRESTORE = FirebaseFirestore.getInstance()
-
-                // Connect to the Cloud Firestore emulator when appropriate. The host '10.0.2.2' is a
-                // special IP address to let the Android emulator connect to 'localhost'.
-                if (sUseEmulators) {
-                    FIRESTORE!!.useEmulator("10.0.2.2", 8080)
-                }
-            }
+            if (FIRESTORE == null) FIRESTORE = FirebaseFirestore.getInstance()
             return FIRESTORE
         }
 
-    // Connect to the Firebase Auth emulator when appropriate. The host '10.0.2.2' is a
-    // special IP address to let the Android emulator connect to 'localhost'.
+    // Connect to the Firebase Auth
     val auth: FirebaseAuth?
         get() {
-            if (AUTH == null) {
-                AUTH = FirebaseAuth.getInstance()
-
-                // Connect to the Firebase Auth emulator when appropriate. The host '10.0.2.2' is a
-                // special IP address to let the Android emulator connect to 'localhost'.
-                if (sUseEmulators) {
-                    AUTH!!.useEmulator("10.0.2.2", 9099)
-                }
-            }
+            if (AUTH == null) AUTH = FirebaseAuth.getInstance()
             return AUTH
         }
 
-    // Connect to the Firebase Auth emulator when appropriate. The host '10.0.2.2' is a
-    // special IP address to let the Android emulator connect to 'localhost'.
+    // Connect to the Firebase Auth
     val authUI: AuthUI?
         get() {
-            if (AUTH_UI == null) {
-                AUTH_UI = AuthUI.getInstance()
-
-                // Connect to the Firebase Auth emulator when appropriate. The host '10.0.2.2' is a
-                // special IP address to let the Android emulator connect to 'localhost'.
-                if (sUseEmulators) {
-                    AUTH_UI.useEmulator("10.0.2.2", 9099)
-                }
-            }
+            if (AUTH_UI == null) AUTH_UI = AuthUI.getInstance()
             return AUTH_UI
         }
 }
