7be6ddb4a18cae63dc9eb892af25895363ad5979
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-12 16:15:29 +0800

    Update FirebaseUtil.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/firebase/FirebaseUtil.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/firebase/FirebaseUtil.kt
@@ -1,40 +1,16 @@
 package com.thepyprogrammer.greenpass.model.firebase
 
-import com.firebase.ui.auth.AuthUI
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.thepyprogrammer.greenpass.BuildConfig
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-
-/**
- * Utility class for initializing Firebase services and connecting them to the Firebase Emulator
- * Suite if necessary.
- */
 object FirebaseUtil {
-    /** Use emulators only in debug builds  */
-    private val sUseEmulators: Boolean = BuildConfig.DEBUG
     private var FIRESTORE: FirebaseFirestore? = null
-    private var AUTH: FirebaseAuth? = null
-    private var AUTH_UI: AuthUI? = null
 
     // Connect to the Cloud Firestore
     val firestore: FirebaseFirestore?
         get() {
-            if (FIRESTORE == null) FIRESTORE = FirebaseFirestore.getInstance()
+            if (FIRESTORE == null) FIRESTORE = Firebase.firestore
             return FIRESTORE
-        }
-
-    // Connect to the Firebase Auth
-    val auth: FirebaseAuth?
-        get() {
-            if (AUTH == null) AUTH = FirebaseAuth.getInstance()
-            return AUTH
-        }
-
-    // Connect to the Firebase Auth
-    val authUI: AuthUI?
-        get() {
-            if (AUTH_UI == null) AUTH_UI = AuthUI.getInstance()
-            return AUTH_UI
         }
 }
