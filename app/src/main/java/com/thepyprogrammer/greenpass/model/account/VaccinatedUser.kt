2a4da256fcbe43e11ff9c0602cf1bbba07ae1050
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-12 16:40:27 +0800

    Rename VaccinatedPerson.kt to VaccinatedUser.kt

---
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedUser.kt
@@ -0,0 +1,10 @@
+package com.thepyprogrammer.greenpass.model.account
+
+import com.google.firebase.Timestamp
+
+data class VaccinatedUser(
+    val fullName: String,
+    val nric: String,
+    val email: String,
+    val dateOfVaccine: Timestamp
+)
