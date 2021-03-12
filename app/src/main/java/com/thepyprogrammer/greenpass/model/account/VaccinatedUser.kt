6df6e83403e9ba454579453b666fa765c8fc5fa5
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-12 22:32:03 +0800

    Update VaccinatedUser.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedUser.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedUser.kt
@@ -3,8 +3,8 @@ package com.thepyprogrammer.greenpass.model.account
 import com.google.firebase.Timestamp
 
 data class VaccinatedUser(
-    val fullName: String,
-    val nric: String,
-    val email: String,
-    val dateOfVaccine: Timestamp
+        val fullName: String,
+        val nric: String,
+        val email: String,
+        val dateOfVaccine: Timestamp
 )
