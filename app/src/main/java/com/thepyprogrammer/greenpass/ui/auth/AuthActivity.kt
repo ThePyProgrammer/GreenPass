135ee80e2ac1ee6f2799daa81a98b0a5b6ab68d3
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-16 01:07:53 +0800

    Update AuthActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/AuthActivity.kt
@@ -5,18 +5,31 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.thepyprogrammer.greenpass.R
 import com.thepyprogrammer.greenpass.ui.main.MainViewModel
 
 class AuthActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: AuthViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val viewPager2 = findViewById<ViewPager2>(R.id.view_pager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            when(position % 2) {
+                0 -> tab.text = "Login"
+                else -> tab.text = "Register"
+            }
+        }.attach()
+        viewPager2.adapter = AuthAdapter(this)
     }
 }
 
@@ -31,3 +44,16 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
     })
 }
+
+
+class AuthAdapter(fa: FragmentActivity): FragmentStateAdapter(fa) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int) =
+            when(position % 2) {
+                0 -> LoginFragment()
+                else -> RegisterFragment()
+            }
+
+}
+
