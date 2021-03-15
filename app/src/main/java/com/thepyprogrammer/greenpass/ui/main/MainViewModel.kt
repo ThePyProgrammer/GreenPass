e80199a527aa390e49631a941b2ad6f61a29ce0b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-16 00:00:53 +0800

    Update MainViewModel.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/MainViewModel.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/MainViewModel.kt
@@ -2,7 +2,7 @@ package com.thepyprogrammer.greenpass.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.thepyprogrammer.greenpass.model.Validate
+import com.thepyprogrammer.greenpass.model.Util
 import java.util.*
 
 class MainViewModel : ViewModel() {
@@ -11,28 +11,18 @@ class MainViewModel : ViewModel() {
 
     var pName = MutableLiveData("name")
     var NRIC = MutableLiveData("IC")
-    var email = MutableLiveData("email")
     var date = MutableLiveData(Date())
 
     fun getResultName() = pName
 
     fun getResultNRIC() = NRIC
 
-    fun getResultEmail() = email
-
 
     fun setNRIC(NRIC: String): Boolean {
-        if (Validate.checkNRIC(NRIC)) {
+        if (Util.checkNRIC(NRIC)) {
             this.NRIC.value = NRIC
         }
-        return NRIC.matches(Validate.nricRegex)
-    }
-
-    fun setEmail(email: String): Boolean {
-        if (Validate.checkEmail(email)) {
-            this.email.value = email
-        }
-        return email.matches(Validate.emailRegex)
+        return NRIC.matches(Util.nricRegex)
     }
 
 
