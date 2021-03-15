c44fc79e3555b512db0187d6008e236cab8fe80a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-16 00:26:07 +0800

    Update FirebaseUtil.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/firebase/FirebaseUtil.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/firebase/FirebaseUtil.kt
@@ -3,9 +3,12 @@ package com.thepyprogrammer.greenpass.model.firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.thepyprogrammer.greenpass.model.account.VaccinatedUser
 
 object FirebaseUtil {
     private var FIRESTORE: FirebaseFirestore? = null
+
+    var user: VaccinatedUser? = null;
 
     // Connect to the Cloud Firestore
     val firestore: FirebaseFirestore?
