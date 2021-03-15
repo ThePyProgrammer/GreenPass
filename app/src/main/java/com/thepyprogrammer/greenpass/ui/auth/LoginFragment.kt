5cd30752279e9451d8784bdae3fa1ceaa4734246
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-15 19:41:47 +0800

    Update LoginFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginFragment.kt
@@ -1,6 +1,132 @@
 package com.thepyprogrammer.greenpass.ui.auth
 
+import android.app.Activity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.thepyprogrammer.greenpass.R
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
+}
 
 class LoginFragment: Fragment() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
+
+
+        val username = root.findViewById<EditText>(R.id.nricInput) //   quickFix
+        val password = root.findViewById<EditText>(R.id.password)
+        val login = root.findViewById<Button>(R.id.login)
+        val loading = root.findViewById<ProgressBar>(R.id.loading)
+
+        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
+                .get(LoginViewModel::class.java)
+
+        loginViewModel.loginFormState.observe(viewLifecycleOwner, Observer {
+            val loginState = it ?: return@Observer
+
+            // disable login button unless both username / password is valid
+            login.isEnabled = loginState.isDataValid
+
+            if (loginState.usernameError != null) {
+                username.error = getString(loginState.usernameError)
+            }
+            if (loginState.passwordError != null) {
+                password.error = getString(loginState.passwordError)
+            }
+        })
+
+        loginViewModel.loginResult.observe(viewLifecycleOwner, Observer {
+            val loginResult = it ?: return@Observer
+
+            loading.visibility = View.GONE
+            if (loginResult.error != null) {
+                showLoginFailed(loginResult.error)
+            }
+            if (loginResult.success != null) {
+                updateUiWithUser(loginResult.success)
+            }
+
+        })
+
+        username.afterTextChanged {
+            loginViewModel.loginDataChanged(
+                    username.text.toString(),
+                    password.text.toString()
+            )
+        }
+
+        password.apply {
+            afterTextChanged {
+                loginViewModel.loginDataChanged(
+                        username.text.toString(),
+                        password.text.toString()
+                )
+            }
+
+            setOnEditorActionListener { _, actionId, _ ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_DONE ->
+                        loginViewModel.login(
+                                username.text.toString(),
+                                password.text.toString()
+                        )
+                }
+                false
+            }
+
+            login.setOnClickListener {
+                loading.visibility = View.VISIBLE
+                loginViewModel.login(username.text.toString(), password.text.toString())
+            }
+        }
+        return root
+    }
+
+    private fun updateUiWithUser(model: LoggedInUserView) {
+        val welcome = getString(R.string.welcome)
+        val displayName = model.displayName
+        // TODO : initiate successful logged in experience
+        Toast.makeText(
+                activity,
+                "$welcome $displayName",
+                Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun showLoginFailed(@StringRes errorString: Int) {
+        Toast.makeText(activity, errorString, Toast.LENGTH_SHORT).show()
+    }
+
 }
