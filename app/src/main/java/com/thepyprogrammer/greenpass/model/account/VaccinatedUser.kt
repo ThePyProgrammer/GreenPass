a56c33f42e36dc4bb7906e4d39befa58f898f017
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-15 22:30:55 +0800

    Update VaccinatedUser.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedUser.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/account/VaccinatedUser.kt
@@ -1,13 +1,11 @@
 package com.thepyprogrammer.greenpass.model.account
 
-import java.io.Serializable
 import com.google.firebase.Timestamp
+import java.io.Serializable
 
 data class VaccinatedUser(
         val nric: String,
         val fullName: String,
         val dateOfVaccine: Timestamp,
         val password: String
-): Serializable {
-
-}
+) : Serializable
