ce7f1108e4a5452bd4b2549ecae433bd76d82d91
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-15 19:41:51 +0800

    Update LoginViewModel.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginViewModel.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginViewModel.kt
@@ -17,9 +17,9 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
     private val _loginResult = MutableLiveData<LoginResult>()
     val loginResult: LiveData<LoginResult> = _loginResult
 
-    fun login(username: String, email: String, password: String) {
+    fun login(username: String, password: String) {
         // can be launched in a separate asynchronous job
-        val result = loginRepository.login(username, email, password)
+        val result = loginRepository.login(username, password)
 
         if (result is Result.Success) {
             _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.fullName))
