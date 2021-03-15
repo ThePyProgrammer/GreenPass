3def7762a7c19a8f10a7561985e5137172fd0688
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-16 00:26:11 +0800

    Update AuthActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/AuthActivity.kt
@@ -17,11 +17,6 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
-
-
-
-
     }
 }
 
