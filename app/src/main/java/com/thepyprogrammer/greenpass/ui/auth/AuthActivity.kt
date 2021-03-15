8bef147feb866472d8b23d14751a6ab10a251ca3
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-15 22:31:06 +0800

    Update AuthActivity.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/AuthActivity.kt
@@ -1,20 +1,10 @@
 package com.thepyprogrammer.greenpass.ui.auth
 
-import android.app.Activity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 import com.thepyprogrammer.greenpass.R
 
 class AuthActivity : AppCompatActivity() {
@@ -24,4 +14,16 @@ class AuthActivity : AppCompatActivity() {
         setContentView(R.layout.activity_auth)
 
     }
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
 }
