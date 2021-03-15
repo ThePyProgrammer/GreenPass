f5462e11fe4eff05b9234ed767d84404f73ef140
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-16 01:07:41 +0800

    Delete LoginResult.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.thepyprogrammer.greenpass.ui.auth
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-        val success: LoggedInUserView? = null,
-        val error: Int? = null
-)
