9ca86a48059b507e05bf41346b6b2275220dc89d
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-16 17:22:26 +0800

    Update Result.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/model/account/Result.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/model/account/Result.kt
@@ -20,15 +20,3 @@ sealed class Result<out T : Any> {
         }
     }
 }
-
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
-}
