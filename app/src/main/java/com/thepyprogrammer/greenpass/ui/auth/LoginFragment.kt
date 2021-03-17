7983261f21fc998f2f002481a79b1e8ccf8e0a6d
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-17 22:55:25 +0800

    Update LoginFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/auth/LoginFragment.kt
@@ -71,6 +71,8 @@ class LoginFragment : Fragment() {
             loading.visibility = View.VISIBLE
             viewModel.login()
 
+            FirebaseUtil.retrieveImage(activity)
+
             startActivityForResult(Intent(activity, MainActivity::class.java).also {
                 it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             }, 0)
