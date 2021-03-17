82ca4f8c2737ec7d4d5c422afe5a30cb841c164c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-17 21:29:03 +0800

    Update SettingsFragment.kt

--- app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/thepyprogrammer/greenpass/ui/main/settings/SettingsFragment.kt
@@ -1,6 +1,10 @@
 package com.thepyprogrammer.greenpass.ui.main.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -8,7 +12,12 @@ import androidx.preference.SwitchPreferenceCompat
 import androidx.transition.TransitionInflater
 import com.thepyprogrammer.greenpass.R
 import com.thepyprogrammer.greenpass.ui.image.EditImage
+import com.thepyprogrammer.greenpass.ui.image.ImageUtil
 import com.thepyprogrammer.greenpass.ui.main.MainActivity
+import java.io.File
+import java.io.IOException
+import java.io.PrintWriter
+import java.util.*
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -37,10 +46,60 @@ class SettingsFragment : PreferenceFragmentCompat() {
             true
         }
 
-        val button: Preference? = findPreference("image")
-        button?.onPreferenceClickListener = Preference.OnPreferenceClickListener { //code for what you want it to do
+        val imageButton: Preference? = findPreference("image")
+        imageButton?.onPreferenceClickListener = Preference.OnPreferenceClickListener { //code for what you want it to do
             activity?.let { it1 -> EditImage(it1, it1) }
             true
         }
+
+        val logout: Preference? = findPreference("logout")
+        logout?.onPreferenceClickListener = Preference.OnPreferenceClickListener { //code for what you want it to do
+            (activity as MainActivity).logout()
+            true
+        }
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ImageUtil.REQUEST_IMAGE) {
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+                val uri: Uri? = data?.getParcelableExtra("path")
+                try {
+                    // You can update this bitmap to your server
+                    MediaStore.Images.Media.getBitmap(activity?.contentResolver, uri)
+
+                    //save uri to internal storage
+                    writeData(uri.toString())
+
+                    //todo fix this one loadProfile(uri.toString())
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    private fun writeData(s: String) {
+        val imageInfoFile = File(activity?.filesDir, "profileImageURI.txt")
+        val output = PrintWriter(imageInfoFile)
+        output.println(s)
+        output.close()
+        println(s)
+    }
+
+    private fun readData(): String {
+        if (!imageInfoFile!!.exists()) {
+            return ""
+        }
+        val scanner = Scanner(imageInfoFile)
+        val string = StringBuilder(scanner.nextLine())
+
+        while (scanner.hasNextLine())
+            string.append("\n" + scanner.nextLine())
+
+
+        scanner.close()
+        return string.toString()
     }
 }
